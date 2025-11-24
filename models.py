90063298b1c59914f1ed62d9b8618d6d22e8a16a
EliSchwartzman <elischwartzman04@gmail.com>
2025-11-23 22:59:15 -0500

    Corrected class Bet

--- models.py
+++ models.py
@@ -20,15 +20,18 @@ class Prediction:
 
 # Bet model
 class Bet:
-    def __init__(self, bet_id, title, description, answer_type, close_at, correct_answer=None, is_open=True, is_resolved=False):
+    def __init__(self, bet_id, created_by_user_id, title, description, answer_type,
+                 is_open, is_resolved, created_at, close_at, resolved_at=None, correct_answer=None):
         self.bet_id = bet_id
+        self.created_by_user_id = created_by_user_id
         self.title = title
         self.description = description
         self.answer_type = answer_type
-        self.close_at = close_at
-        self.correct_answer = correct_answer
         self.is_open = is_open
         self.is_resolved = is_resolved
-        self.created_at = None
+        self.created_at = created_at
+        self.close_at = close_at
+        self.resolved_at = resolved_at
+        self.correct_answer = correct_answer
 
 
