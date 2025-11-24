dcca025d6d1e165537c75a01f7d846c5ca7c4482
EliSchwartzman <elischwartzman04@gmail.com>
2025-11-23 23:56:18 -0500

    sidebar pt.5

--- streamlit_app.py
+++ streamlit_app.py
@@ -358,7 +358,7 @@ def main_panel():
     user = st.session_state.user
     st.sidebar.title("Reedz Betting Menu")
     st.sidebar.markdown(
-        f"**Username:** {user.username}\n **Role:** {user.role}"
+        f"**Username:** {user.username}\n\n **Role:** {user.role}"
         )
     st.sidebar.divider()
     pages = [
